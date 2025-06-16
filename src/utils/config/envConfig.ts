49464cc86d76e6681b0f1c168828f5f009c75663
Nick742037091 <742037091@qq.com>
2025-06-16 16:15:53 +0800

    ✨ feat: 调整环境变量配置

--- src/utils/config/envConfig.ts
+++ src/utils/config/envConfig.ts
@@ -3,17 +3,12 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 function parseEnv() {
-  const localEnv = path.resolve('.env.local');
-  const prodEnv = path.resolve('.env.prod');
+  const env = path.resolve('.env');
 
-  if (!fs.existsSync(localEnv) && !fs.existsSync(prodEnv))
-    throw new Error('缺少环境配置文件');
-
-  const filePath = isProd && fs.existsSync(prodEnv) ? prodEnv : localEnv;
-  return { path: filePath };
+  if (!fs.existsSync(env)) throw new Error('缺少环境配置文件');
+  
+  return { path: env };
 }
 
 export default parseEnv();
